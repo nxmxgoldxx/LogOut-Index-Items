9e9f9b1fa0f8a7aed6e1fd68306afdd24283744a
Calenté Cardwell <nxmxgoldxx@live.com>
2021-07-21 00:28:03 -0700

    Removed Item(s) (2) Items removed 'books' and 'cats'

--- js/script.js
+++ js/script.js
@@ -11,9 +11,25 @@ var stuff = [
 ];
 
 // Log out even index items
-stuff.forEacg(function (item, index) {
-  // log out items with even index
-  if (index % 2 === 0) {
-    console.log(item);
+// stuff.forEacg(function (item, index) {
+// log out items with even index
+// if (index % 2 === 0) {
+// console.log(item);
+// }
+// });
+
+var removeItem = function (array, item) {
+  var index = array.indexOf(item);
+
+  if (index === -1) {
+    console.log("No such element exists in the array.");
+  } else {
+    console.log(`Removing ${item}`);
+    array.splice(index, 1);
   }
-});
+};
+
+removeItem(stuff, "books");
+console.log(stuff);
+removeItem(stuff, "cats");
+console.log(stuff);
